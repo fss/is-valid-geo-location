14c88fc719da2b7fd0e8974b5a0b403ca96957ed
Rafal Drag <rd@fss.cc>
2024-01-22 09:55:42 +0100

    fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,3 @@
 export function isValidLatitude (value: any): boolean
 export function isValidLongitude (value: any): boolean
-export function isValidGeolocation (location: { lat: number, lng: number }): boolean
+export function isValidGeolocation (location: { lat: any, lng: any }): boolean
