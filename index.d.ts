4bca7431a6ef43874c209a4612af0e1f0650b090
Rafal Drag <rd@fss.cc>
2024-01-22 09:31:37 +0100

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export function isValidLatitude (value: any): boolean
+export function isValidLongitude (value: any): boolean
+export function isValidGeolocation (location: { lat: number, lng: number }): boolean
